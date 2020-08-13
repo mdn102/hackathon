eafb973a2a6883f8a01d81392833a41e5fb082bb
Jameson Aardalen <jamesonaardalen@Jamesons-MBP.attlocal.net>
2020-08-13 10:06:45 -0700

    Fixed nav bar

--- terra/components/Nav.js
+++ terra/components/Nav.js
@@ -20,21 +20,9 @@ const nav = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="mr-auto">
                     <Link href="/about" passHref><Nav.Link>About</Nav.Link></Link>
-                    <NavDropdown title="Places" id="basic-nav-dropdown">
-                      <Link href="/beach" passHref><NavDropdown.Item>Beach</NavDropdown.Item></Link>
-                      <Link href="/city" passHref><NavDropdown.Item>City</NavDropdown.Item></Link>
-                      <Link href="/desert" passHref><NavDropdown.Item>Desert</NavDropdown.Item></Link>
-                      <Link href="/jungle" passHref><NavDropdown.Item>Jungle</NavDropdown.Item></Link>
-                      <Link href="/ocean" passHref><NavDropdown.Item>Ocean</NavDropdown.Item></Link>
-                      <Link href="/forest" passHref><NavDropdown.Item>Forest</NavDropdown.Item></Link>
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item href="#action/3.4">How to help</NavDropdown.Item>
-                    </NavDropdown>
+                    <Link href="/about" passHref><Nav.Link>Resources</Nav.Link></Link>
+                    <Link href="/about" passHref><Nav.Link>Donate</Nav.Link></Link>
                   </Nav>
-                  <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                  </Form>
                 </Navbar.Collapse>
             </Navbar>
           </Col>
